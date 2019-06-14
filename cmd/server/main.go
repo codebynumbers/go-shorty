3476b253b5989719ebd03cc8d79b30c82a2f350e
Rob Harrigan <rharrigan@50onred.com>
2019-06-14 13:02:12 -0400

    Use envconfig

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,10 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"fmt"
+	"github.com/codebynumbers/go-shorty/internal/configuration"
 	"github.com/go-redis/redis"
 	"github.com/julienschmidt/httprouter"
+	"github.com/kelseyhightower/envconfig"
 	_ "github.com/mattn/go-sqlite3"
 	"hash/fnv"
 	"html/template"
@@ -15,39 +17,36 @@ import (
 	"strings"
 )
 
-// Config
-const port = "3000"
-const domain = "localhost"
-const dbDriver = "sqlite3"
-const dbPath = "./shorty.db"
-
-var servingDomain = fmt.Sprintf("%s:%s", domain, port)
-
-var client = redis.NewClient(&redis.Options{
-	Addr:     "localhost:16379", // for local docker-compose
-	Password: "",               // no password set
-	DB:       0,                // use default DB
-})
-
+var servingDomain string
+var client *redis.Client
 var db *sql.DB
+var config configuration.Config
 
 type ResultPageData struct {
 	ShortenedUrl string
 }
 
 func main() {
-	initDb()
+	var err error
+
+	if err = envconfig.Process("GOSHORTY", &config); err != nil {
+		log.Fatal(err)
+	}
+
+	initDbs()
 	router := httprouter.New()
 	router.GET("/", indexHandler)
 	router.GET("/:tag", expandHandler)
 	router.POST("/data/shorten/", shortenHandler)
+
+	servingDomain = fmt.Sprintf("%s:%s", config.Domain, config.HostPort)
 	log.Println(fmt.Sprintf("Listening on %s...", servingDomain))
 	http.ListenAndServe(servingDomain, router)
 }
 
-func initDb() {
+func initDbs() {
 	var err error
-	db, err = sql.Open(dbDriver, dbPath)
+	db, err = sql.Open(config.DbDriver, config.DbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,6 +54,10 @@ func initDb() {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+
+	client = redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%s", config.RedisHost, config.RedisPort),
+	})
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
